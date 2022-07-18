e757f15dbcdd3e2620cfbd4661e17663caa626f0
Azamat <kastoev2001@gmail.com>
2022-07-18 12:52:00 +0300

    Добавил страниуц 404, и компонент.

---
+++ project/src/pages/not-page/not-page.tsx
@@ -0,0 +1,11 @@
+function NotPageScreen(): JSX.Element {
+  return (
+    <div className="not-page">
+      <div className="not-page__content">
+        <a href="#">Перейти к главной странице</a>
+      </div>
+    </div>
+  );
+}
+
+export default NotPageScreen;
