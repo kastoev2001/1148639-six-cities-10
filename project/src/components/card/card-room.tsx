e5ea4b8f7634039326315aebbf7948edcc631e3d
Azamat <kastoev2001@gmail.com>
2022-07-18 13:03:31 +0300

    Измернил директорию ./prject/src/component/card.tsx на ./prject/src/component/card/card-room.tsx

--- project/src/components/card/card-room.tsx
+++ project/src/components/card/card-room.tsx
@@ -1,4 +1,4 @@
-function Card(): JSX.Element {
+function CardRoom(): JSX.Element {
   return (
     <article className="cities__card place-card">
       <div className="place-card__mark">
@@ -37,4 +37,4 @@ function Card(): JSX.Element {
   );
 }
 
-export default Card;
+export default CardRoom;
