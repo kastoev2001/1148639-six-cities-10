2e9161ea88d0a8ef36ee19eea0b63afc399ed4ca
Azamat <kastoev2001@gmail.com>
2022-08-06 02:04:41 +0300

    3. Инициализировал хранилище (store). Обернул компонент App в Provider и передал props store (хранилище).

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
 import { offers } from './mocks/offers';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const COUNT_ROOMS = 312;
 
@@ -12,6 +14,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
+		<Provider store={store}>
     <App countRooms={COUNT_ROOMS} offers={offers}/>
+		</Provider>
   </React.StrictMode>,
 );
