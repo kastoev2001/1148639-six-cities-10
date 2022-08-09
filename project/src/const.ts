11aef0ccb277e0e59b5694092c668134f54d7fe8
Azamat <kastoev2001@gmail.com>
2022-08-09 13:57:54 +0300

    Удалил константу locationCity. Приминил всем константам правила CONST_CASE.

--- project/src/const.ts
+++ project/src/const.ts
@@ -2,53 +2,44 @@ import { LocationCity } from './types/offers';
 import { Icon } from 'leaflet';
 
 export enum AppRoute {
-  Root = '/',
-  Favorites = '/favorites',
-  NotFavorites = '/not-favorites',
-  Login = '/login',
-  Offer = '/offer'
+	Root = '/',
+	Favorites = '/favorites',
+	NotFavorites = '/not-favorites',
+	Login = '/login',
+	Offer = '/offer'
 }
 
 export enum AuthorizationStatus {
-  Auth = 'AUTH',
-  NoAuth = 'NO_AUTH',
-  Unknown = 'UNKNOWN',
+	Auth = 'AUTH',
+	NoAuth = 'NO_AUTH',
+	Unknown = 'UNKNOWN',
 }
 
 export enum CardStatus {
-  ACTIVE = '0.6',
-  NO_ACTIVE = '1',
+	ACTIVE = '0.6',
+	NO_ACTIVE = '1',
 }
 
 export enum RatingStatus {
-  PERFECT = 5,
-  GOOD = 4,
-  NOT_BAD = 3,
-  BADLY = 2,
-  TERRIBLY = 1,
+	PERFECT = 5,
+	GOOD = 4,
+	NOT_BAD = 3,
+	BADLY = 2,
+	TERRIBLY = 1,
 }
 
 export const FIRST_STATE_RATING = 0;
 
-export const locationCity: LocationCity = {
-  location: {
-    latitude: 52.36862421982035,
-    longitude: 4.903369107744523,
-    zoom: 10,
-  },
-  name: 'Paris',
-};
-
-export const currentCustomIcon = new Icon({
-  iconUrl: './img/pin.svg',
-  iconSize: [27, 39],
+export const CURRENT_CUSTOM_ICON = new Icon({
+	iconUrl: './img/pin.svg',
+	iconSize: [27, 39],
 });
 
-export const cities = [
-  'Paris',
-  'Cologne',
-  'Brussels',
-  'Amsterdam',
-  'Hamburg',
-  'Dusseldorf',
+export const CITIES = [
+	'Paris',
+	'Cologne',
+	'Brussels',
+	'Amsterdam',
+	'Hamburg',
+	'Dusseldorf',
 ];
