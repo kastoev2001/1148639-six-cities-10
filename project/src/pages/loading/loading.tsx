85a9ea976f2ea112d3d31b80f72522585062e098
Azamat <=>
2022-08-15 11:35:17 +0300

    Создал модуль для компонента Loading.

---
+++ project/src/pages/loading/loading.tsx
@@ -0,0 +1,7 @@
+function Loading(): JSX.Element {
+  return (
+    <h1 style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>Loading...</h1>
+  );
+}
+
+export default Loading;
