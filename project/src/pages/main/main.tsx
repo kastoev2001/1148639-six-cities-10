dc0193399a16a81c9eaa079c27c7cd0090ea3e75
Azamat <kastoev2001@gmail.com>
2022-08-27 14:32:12 +0300

    1. Создал компонент сортировки.

--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -11,6 +11,7 @@ import { Offer } from '../../types/offers';
 import { changeCity } from '../../store/city-data/city-data';
 import { getOffers } from '../../store/offers-process/offers-selector';
 import { getActiveCity } from '../../store/city-data/city-selector';
+import SortForm from '../../components/main/sort-form/sort-form';
 
 function Main(): JSX.Element {
   const offers = useAppSelector(getOffers);
@@ -55,21 +56,8 @@ function Main(): JSX.Element {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{countRooms} {countRooms > 1 ? 'places' : 'place'} to stay in {activeCity.name}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                  <li className="places__option" tabIndex={0}>Price: low to high</li>
-                  <li className="places__option" tabIndex={0}>Price: high to low</li>
-                  <li className="places__option" tabIndex={0}>Top rated first</li>
-                </ul>
-              </form>
+              
+							<SortForm />
 
               <ListRooms offersFiltred={offersFilterd} />
 
