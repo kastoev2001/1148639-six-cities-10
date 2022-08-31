3808f45cc583b2f2c91d4360334a343dc0a99fb7
Azamat <kastoev2001@gmail.com>
2022-08-31 10:48:24 +0300

    Исправил валидность пороля.

--- project/src/utils/commands.ts
+++ project/src/utils/commands.ts
@@ -5,7 +5,7 @@ export const isLoginCheck = (loginElement: HTMLInputElement): boolean =>
   /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/.test(loginElement.value);
 
 export const isPasswordCheck = (passwordElement: HTMLInputElement): boolean =>
-  /^[а-яА-ЯёЁa-zA-Z0-9]+$/.test(passwordElement.value);
+  /^.*(?=.{1,})(?=.*[a-zA-Z])(?=.*\d)/.test(passwordElement.value);
 
 export const isAxiosError = (error: any): error is AxiosError =>
   typeof error.response.data.error === 'string';
